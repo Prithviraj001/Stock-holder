72081998e03b918a969329202666e4d58c4f5b96
Prithviraj001 <38152947+Prithviraj001@users.noreply.github.com>
2018-04-07 18:57:27 +0530

    Stock holder simulator
    Stock holder simulator that  infer stock data graphically.

---
+++ Stock_Simulator/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+
+namespace Stock_Simulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2015",
+                    Values = new ChartValues<double> { 10, 50, 39, 50 }
+                }
+            };
+
+            //adding series will update and animate the chart automatically
+            SeriesCollection.Add(new ColumnSeries
+            {
+                Title = "2016",
+                Values = new ChartValues<double> { 11, 56, 42 }
+            });
+
+            //also adding values updates and animates the chart automatically
+            SeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Formatter = value => value.ToString("N");
+
+            DataContext = this;
+
+            SeriesCollection1 = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4.3, 6, 5.7, 2 ,4.1 }
+                }
+            };
+
+            Labels1 = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            DataContext = this;
+
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
+
+            SeriesCollection2 = new SeriesCollection
+            {
+                new ScatterSeries
+                {
+                    Values = new ChartValues<ScatterPoint>
+                    {
+                        new ScatterPoint(5, 5, 20),
+                        new ScatterPoint(3, 4, 80),
+                        new ScatterPoint(7, 2, 20),
+                        new ScatterPoint(2, 6, 60),
+                        new ScatterPoint(8, 2, 70)
+                    },
+                    MinPointShapeDiameter = 15,
+                    MaxPointShapeDiameter = 45
+                },
+                new ScatterSeries
+                {
+                    Values = new ChartValues<ScatterPoint>
+                    {
+                        new ScatterPoint(7, 5, 1),
+                        new ScatterPoint(2, 2, 1),
+                        new ScatterPoint(1, 1, 1),
+                        new ScatterPoint(6, 3, 1),
+                        new ScatterPoint(8, 8, 1)
+                    },
+                    PointGeometry = DefaultGeometries.Triangle,
+                    MinPointShapeDiameter = 15,
+                    MaxPointShapeDiameter = 45
+                }
+            };
+
+            DataContext = this;
+
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
+        public SeriesCollection SeriesCollection1 { get; set; }
+        public string[] Labels1 { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        public SeriesCollection SeriesCollection2 { get; set; }
+
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
+
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries)chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
+        }
+
+        private void UpdateAllOnClick(object sender, RoutedEventArgs e)
+        {
+            var r = new Random();
+            foreach (var series in SeriesCollection2)
+            {
+                foreach (var bubble in series.Values.Cast<ScatterPoint>())
+                {
+                    bubble.X = r.NextDouble() * 10;
+                    bubble.Y = r.NextDouble() * 10;
+                    bubble.Weight = r.NextDouble() * 10;
+                }
+            }
+        }
+    }
+}
---
+++ Stock_Simulator/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Simulator
+{
+    public class User
+    {
+        private string FirstName, LastName;
+
+        public User()
+        {
+
+        }
+
+        public User(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+    }
+}
